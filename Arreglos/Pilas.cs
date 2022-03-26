8b9b989bde4b4d064bdd4f037478b4128250f7e9
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-03-26 12:31:39 -0700

    Validacion de Pila del array si esta lleno y vacio

---
+++ Arreglos/Pilas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class Pilas
+    {
+        //Variables que se usaran todo el tiempo en el constructor
+        private string[] array;
+        private int max;
+        private int tope;
+
+        //int tamanio especifica el tamano del array
+        public Pilas(int tamanio)
+        {
+            array = new string[tamanio];
+            max = array.Length - 1;
+            tope = 0;
+        }
+        private bool ValidaVacio() 
+        {
+            //if (tope < 1)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return (tope < 1);
+        }
+
+        private bool ValidaLleno()
+        {
+            return (tope > max);
+        }
+    }
+}
