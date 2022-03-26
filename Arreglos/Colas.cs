1dbec59a04ba5b53910a33afa2f7ed6cb563928e
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-03-26 13:58:22 -0700

    Array colas

---
+++ Arreglos/Colas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class Colas
+    {
+        private string[] array;
+        private int max;
+        private int principio;
+        private int final;
+
+        public Colas(int tamanio)
+        {
+            array = new string[tamanio];
+            max = array.Length - 1;
+            principio = 0;
+            final = 0;
+        }
+    }
+}
