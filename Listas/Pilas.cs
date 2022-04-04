7fad30512c97b98b253b30cc7d03ad5c6e96071c
Jose Carlos Rodriguez <rodjosecarlos15@gmail.com>
2022-04-03 17:06:55 -0700

    Eliminar e Imprimir agregados

--- Listas/Pilas.cs
+++ Listas/Pilas.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Listas
 {
-    internal class Pilas
+    public class Pilas
     {
+        private List<string> list;
+        public Pilas()
+        {
+            list = new List<string>();
+        }
+
+        public void Agregar(string dato)
+        {
+            list.Add(dato);
+        }
+
+        private bool ValidaVacio()
+        {
+            return list.Count == 0;
+        }
+
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("Lista Vacia"):
+            }
+
+            Listas.RemoveAt(Listas.Count - 1);
+        }
+
+        public string Imprimir()
+        {
+            string datos = string.Empty;
+
+            if (ValidaVacio())
+            {
+                return "Lista vacia";
+            }
+
+            
+        }
     }
+    
 }
